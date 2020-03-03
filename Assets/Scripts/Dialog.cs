b1b21b2e3d81ca723707211d90397e50b95f0ef5
ebertozz <elena.bertozzi@quinnipiac.edu>
2020-03-03 07:51:13 -0500

    Added Audio
    This branch adds audio clips to the Dialog class and then the Dialog manager plays them.

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -9,4 +9,6 @@ public class Dialog {   // does not extend monodevelop
 	public string name;  // these will show up in the inspector when attached to a monodevelop script
 	[TextArea(3,10)]		// sets the size of text area in inspector
 	public string[] sentences;  // can add as many sentences as desired
+
+	public AudioClip[] audioClips;  //for audio files 
 }
--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -6,46 +6,66 @@ using UnityEngine.UI;
 public class DialogManager : MonoBehaviour {
 
 	public Text speakerName;
-	public Text convoContent; 
+	public Text convoContent;
 	public GameObject dialogPanel;
 
 	private Queue<string> sentences;
+	private Queue<AudioClip> myAudioClips;
+
 
 	// Use this for initialization
-	void Start () {
-		sentences = new Queue<string> ();	
+	void Start() {
+		sentences = new Queue<string>();
+		myAudioClips = new Queue<AudioClip>();
 	}
-	
-	public void StartDialog(Dialog dialog){  //method called at start of a conversation
-		Debug.Log ("talk to " + dialog.name); // check to make sure this works
+
+	public void StartDialog(Dialog dialog) {  //method called at start of a conversation
+		Debug.Log("talk to " + dialog.name); // check to make sure this works
 		speakerName.text = dialog.name;
 
 		// first need to clear out any previous conversation that might linger in sentences array
 		sentences.Clear();
 
 		// then loop through the array and line up each sentence currently in it to prepare 
-		foreach(string sentence in dialog.sentences){
+		foreach (string sentence in dialog.sentences) {
 
-			sentences.Enqueue (sentence); // put each in the queue
+			sentences.Enqueue(sentence); // put each in the queue
+
 		}
-		// then call a method to actually display it
 
-		DisplayNextSentence ();
+        foreach(AudioClip myClip in dialog.audioClips)
+        {
+			myAudioClips.Enqueue(myClip);
+        }
+
+		DisplayNextSentence();
+		
 	}
 
-	public void DisplayNextSentence (){
-	
+	public void DisplayNextSentence() {
+
 		// first check to see if we are at the end of convo and if so call end method
 
 		if (sentences.Count == 0) { // if array is empty
-		
-			EndConvo (); // call end method
-			return;		// leave the function
+
+			EndConvo(); // call end method
+			return;     // leave the function
 		}
-		string sentence = sentences.Dequeue ();  // otherwise pull sentence out of the queue
+		string sentence = sentences.Dequeue();  // otherwise pull sentence out of the queue
 		convoContent.text = sentence;
 		//Debug.Log ("line is" + sentence); // display it
-	
+
+		PlayDialogAudio();
+
+	}
+
+	public void PlayDialogAudio()
+    {
+		AudioClip myClip = myAudioClips.Dequeue();
+		AudioSource myAudio = GetComponent<AudioSource>();
+		myAudio.clip = myClip;
+		myAudio.Play();
+
 	}
 
 	public void EndConvo (){
